69e14ea93bf3f4daa6e64096c194fa9691e74655
Ridho Bayu Prihatmoko <44189235+Rbp901@users.noreply.github.com>
2018-10-26 08:50:28 +0700

    Add files via upload

---
+++ tugasloop3.php
@@ -0,0 +1,6 @@
+<?php
+for ($i=5;$i<=17;$i+=3)
+{
+echo"<br>bilangan ke $i";
+}
+?>
---
+++ tugasloop5.php
@@ -0,0 +1,18 @@
+<?php
+
+$jumlah= 0;
+
+for ($i=1; $i<10; $i+=2){
+
+echo "$i+";
+
+$jumlah += $i;
+
+}
+
+
+echo "totalnya = $jumlah";
+
+
+
+?>
